d902c25dfa6d7e75382d582fea3ffa3612139a29
jyothi-morla23 <jyothim2303@gmail.com>
2024-06-27 21:50:21 +0530

    page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,81 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pages';
+  currentPage = 0;
+  images = [
+    {
+      title: 'At the beach',
+      url: './assets/beach.jpg',
+    },
+    {
+      title: 'At the forest',
+      url: './assets/forest.jpeg',
+    },
+    {
+      title: 'At the sunrise',
+      url: './assets/sunrise.jpeg',
+    },
+    {
+      title: 'At the waterfal',
+      url: './assets/waterfal.jpeg',
+    },
+    {
+      title: 'At the beach',
+      url: './assets/beach.jpg',
+    },
+    {
+      title: 'At the forest',
+      url: './assets/forest.jpeg',
+    },
+    {
+      title: 'At the sunrise',
+      url: './assets/sunrise.jpeg',
+    },
+    {
+      title: 'At the waterfal',
+      url: './assets/waterfal.jpeg',
+    },
+    {
+      title: 'At the beach',
+      url: './assets/beach.jpg',
+    },
+    {
+      title: 'At the forest',
+      url: './assets/forest.jpeg',
+    },
+    {
+      title: 'At the sunrise',
+      url: './assets/sunrise.jpeg',
+    },
+    {
+      title: 'At the waterfal',
+      url: './assets/waterfal.jpeg',
+    },
+  ];
+
+  checkWindowIndex(index: number) {
+    return Math.abs(this.currentPage - index) < 5;
+  }
+
+  // getClass(i: number) {
+  //   const classes = [];
+
+  //   if (something()) {
+  //     classes.push('active');
+  //   }
+  //   if (somethingElse()) {
+  //     classes.push('someOtherClass');
+  //   }
+  //   if (somethingYetElse()) {
+  //     classes.push('other');
+  //   }
+
+  //   if (yetAnotherCheck()) {
+  //     classes.push('check');
+  //   }
+  //   return classes;
+  // }
 }
